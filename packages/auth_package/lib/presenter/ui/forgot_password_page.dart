c2392d081f2706e3683c35fbb55e7c6f7c889b78
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2023-06-16 01:53:34 -0300

    fix forgot pw layout

--- packages/auth_package/lib/presenter/ui/forgot_password_page.dart
+++ packages/auth_package/lib/presenter/ui/forgot_password_page.dart
@@ -1,3 +1,4 @@
+import 'package:auth_package/presenter/ui/widgets/body_container.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'widgets/text_field_custom.dart';
@@ -18,28 +19,23 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SizedBox.expand(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CachedNetworkImage(
-                imageUrl: 'https://d3ebnpochj0915.cloudfront.net/dev_logo.png',
-              ),
-              const SizedBox(height: 16),
-              const TextFieldCustom(
-                // onChanged: store.setEmail,
-                text: 'Email',
-                prefixIcon: Icon(Icons.email_outlined),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                  onPressed: () {}, child: const Text('Reset My Password'))
-            ],
+        body: BodyContainer(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CachedNetworkImage(
+            imageUrl: 'https://d3ebnpochj0915.cloudfront.net/dev_logo.png',
           ),
-        ),
+          const SizedBox(height: 16),
+          const TextFieldCustom(
+            // onChanged: store.setEmail,
+            text: 'Email',
+            prefixIcon: Icon(Icons.email_outlined),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+              onPressed: () {}, child: const Text('Reset My Password'))
+        ],
       ),
     ));
   }
