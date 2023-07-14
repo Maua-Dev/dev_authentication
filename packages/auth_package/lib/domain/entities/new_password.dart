7b2d46d7942d2e7c01b39f59e4e5819d470fe50c
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2023-07-14 18:31:00 -0300

    minor fixes

---
+++ packages/auth_package/lib/domain/entities/new_password.dart
@@ -0,0 +1,22 @@
+class NewPassword {
+  final String email;
+  final String code;
+  final String newPassword;
+  final String confirmPassword;
+
+  NewPassword(
+      {required this.email,
+      required this.code,
+      required this.newPassword,
+      required this.confirmPassword});
+
+  bool get isValidCode => code.isNotEmpty;
+  bool get isValidNewPassword =>
+      newPassword.isNotEmpty &&
+      newPassword.length > 7 &&
+      newPassword.contains(RegExp(r'[A-Z]')) &&
+      newPassword.contains(RegExp(r'[a-z]')) &&
+      newPassword.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>]'));
+  bool get isValidConfirmPassword =>
+      isValidNewPassword && confirmPassword == newPassword;
+}
