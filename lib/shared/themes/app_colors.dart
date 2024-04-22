0e48f83ef9b5f024a7479d1f46f0927e65e11c1f
EnricoSantarelli <enricosantarelli@hotmail.com>
2024-04-22 20:29:42 -0300

    fix blue color

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color get primary => const Color(0xFFBA2512);
-  static Color get accent => Colors.blueAccent;
+  static Color get accent => const Color(0xFF270272);
   static MaterialColor get primaryMaterial {
     return createMaterialColor(primary);
   }
